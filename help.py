f2e20d270d81253d11a0886eab5f5d7dbb7abb42
Mantarain <mantas.masaitis@isfwaterloo.org>
2023-04-14 09:35:08 +0200

    string fixes

--- help.py
+++ help.py
@@ -4,12 +4,12 @@ def help():
     return """
         help            - prints out each command.
         clear           - clears the text out of the terminal.
-        exit            - closes current session.
+        exit            - closes current session and overwrites save
         archive         - shows the archive list
         todo            - shows the current todo list
         open            - opens todo save file to continue a session
 
-        new item                            - creates a new task with name and details inpu.
-        remove item                         - removes item with the given name.
-        done item                           - archives item into an archive list (accessed with the "archive" command)
+        new item                            - creates a new task with name and details input.
+        remove item                         - deletes item with the given name.
+        done item                           - archives item into the archive list (accessed with the "archive" command)
             """
